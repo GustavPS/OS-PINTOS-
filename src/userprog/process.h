9328764ef37f26c66e75280a8adfdd3017d83f47
Gustav P Svensson <gustavps97@gmail.com>
2019-04-10 11:42:37 +0200

    Deluppgift 11 klar

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -3,6 +3,26 @@
 
 #include "threads/thread.h"
 
+struct main_args
+{
+  /* Hint: When try to interpret C-declarations, read from right to
+   * left! It is often easier to get the correct interpretation,
+   * altough it does not always work. */
+
+  /* Variable "ret" that stores address (*ret) to a function taking no
+   * parameters (void) and returning nothing. */
+  void (*ret)(void);
+
+  /* Just a normal integer. */
+  int argc;
+
+  /* Variable "argv" that stores address to an address storing char.
+   * That is: argv is a pointer to char*
+   */
+  char** argv;
+};
+
+
 void process_init (void);
 void process_print_list (void);
 void process_exit (int status);
@@ -10,6 +30,12 @@ tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_cleanup (void);
 void process_activate (void);
+
+void* setup_main_stack(const char*, void*);
+int count_args(const char*, const char*);
+bool exists_in(char, const char*);
+
+
 
 /* This is unacceptable solutions. */
 #define INFINITE_WAIT() for ( ; ; ) thread_yield()
@@ -21,3 +47,4 @@ void process_activate (void);
     } while ( 0 )
 
 #endif /* userprog/process.h */
+
